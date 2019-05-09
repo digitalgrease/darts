b2b41ee9bfd4aa6a7202a59e8100d7c04789ec84
Tom <tom@digitalgrease.co.uk>
2019-05-09 10:55:17 +0200

    Testing service worker

--- sw.js
+++ sw.js
@@ -1,19 +1,28 @@
-const preCacheName = 'preCache';
-const preCacheFiles = ['/'];
+const preCacheName = 'pre-cache-darts-scoreboard';
+const preCacheFiles = [
+    '/',
+    'vue.js'
+];
 
 self.addEventListener('install', event => {
+    console.log( "installing precache files" );
+    
     caches.open(preCacheName).then(function (cache) {
         return cache.addAll(preCacheFiles);
     });
 });
 
 self.addEventListener('fetch', event => {
+    console.log( "fetching" );
+    
     event.respondWith(
         caches.match(event.request).then(response => {
             if (!response) {
+                console.log('falling back to network fetch');
                 // Fall back to the network fetch.
                 return fetch(event.request);
             }
+            console.log('returning response');
             return response;
         })
     )
